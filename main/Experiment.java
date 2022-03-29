2a147da57e6465208cdc4dd54c5c96bd48e6c9c6
NinjaDino13 <64268525+NinjaDino13@users.noreply.github.com>
2022-03-29 13:53:39 -0400

    Added Locking Experiment

--- main/Experiment.java
+++ main/Experiment.java
@@ -30,7 +30,32 @@ public class Experiment {
             removal_failures += standard[i].removal_failures;
         }
         System.out.println(String.format("Insertion failed %d times, and deletion failed %d times.", insertion_failures, removal_failures));
-        System.out.println("Finished threaded experiment on standard hash table.");
+        System.out.println("Finished threaded experiment on standard hash table.\n");
+
+
+        ThreadedExperiment[] locking_experiment = new ThreadedExperiment[8];
+        Thread[] locking = new Thread[8];
+
+        for (int i = 0; i < 8; i++) {
+            locking_experiment[i] = new ThreadedExperiment(i);
+            locking[i] = new Thread(locking_experiment[i], Integer.toString(i));
+        }
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < 8; i++) locking[i].start();
+        for (int i = 0; i < 8; i++) locking[i].join();
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+
+        System.out.println(String.format("Threaded locking hashtable took %d milliseconds to perform 64708 inserts, 129416 searchs, and 64708 deletes", duration/1000000));
+        insertion_failures = 0;
+        removal_failures = 0;
+        for (int i = 0; i < 8; i++) {
+            insertion_failures += locking_experiment[i].insertion_failures;
+            removal_failures += locking_experiment[i].removal_failures;
+        }
+        System.out.println(String.format("Insertion failed %d times, and deletion failed %d times.", insertion_failures, removal_failures));
+        System.out.println("Finished threaded experiment on the locking hash table.");
     }
 }
 
@@ -76,6 +101,35 @@ class ThreadedExperiment implements Runnable {
 
     ThreadedExperiment(int thread_num) {
         this.standard_unthreaded = new HashTable();
+        this.insertion_failures  = 0;
+        this.removal_failures = 0;
+        this.thread_num = thread_num;
+    }
+
+    public void run() {
+        for (int i = (this.thread_num * 8089); i < (this.thread_num * 8089) + 8089; i++) {
+            standard_unthreaded.put(i);
+        }
+        for (int i = (this.thread_num * 8089); i < (this.thread_num * 8089) + 8089; i++) {
+            if (!standard_unthreaded.search(i)) insertion_failures++;
+        }
+        for (int i = (this.thread_num * 8089); i < (this.thread_num * 8089) + 8089; i++) {
+            standard_unthreaded.remove(i);
+        }
+        for (int i = (this.thread_num * 8089); i < (this.thread_num * 8089) + 8089; i++) {
+            if (standard_unthreaded.search(i)) removal_failures++;
+        }
+    }
+}
+
+class ThreadedExperiment2 implements Runnable {
+    LockingHashTable standard_unthreaded;
+    public int insertion_failures; // How many times the insertion of a key-value failed.
+    public int removal_failures; // How many times the removal of a key-value failed.
+    int thread_num;
+
+    ThreadedExperiment2(int thread_num) {
+        this.standard_unthreaded = new LockingHashTable();
         this.insertion_failures  = 0;
         this.removal_failures = 0;
         this.thread_num = thread_num;
