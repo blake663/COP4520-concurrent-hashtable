d98ceaa69f7f8bfdb3c3069ef4dd972dfa56927e
Dalton <daltonkajander@knights.ucf.edu>
2022-04-28 20:15:08 -0400

    Added experimental data

--- main/evaluation.py
+++ main/evaluation.py
@@ -1,33 +1,33 @@
-import platform
-import numpy as np
+# Modules
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 importedCPU = True
 try:
 	import cpuinfo
 except:
 	importedCPU = False
 
+# Variables
 filename = "experimentalTimes.csv"
 df = pd.read_csv(filename)
 linespace = range(1, len(df) + 1)
 names = df.columns
 rows, cols = 2, 2
 index = 0
+totalTime = 0
 
+# Showing data
 if (importedCPU):
 	print(cpuinfo.cpu.info[0]["ProcessorNameString"])
 else:
 	print("Could not record CPU information")
-	
-
 print("Trials: " + str(len(df)))
 for name in names:
+	totalTime += df[name].sum()
 	print("{0}:\n\tAverage: {1} ms\n\tHigh: {2} ms\n\tLow: {3} ms\n\tStandard deviation: {4} ms".
 		format(name, round(df[name].mean()), df[name].max(), df[name].min(), round(df[name].std(), 1)))
-
+print("Total time to run {0} tests: {1} ms".format(str(len(df)), totalTime))
 fig, axs = plt.subplots(ncols = cols, nrows = rows)
 for col in range(cols):
 	for row in range(rows):
